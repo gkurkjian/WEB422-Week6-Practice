97ba93e52dad894e018c0fd56c575a57f5d1909b
gkurkjian <gkurkjian@myseneca.ca>
2023-05-01 12:07:13 -0400

    6th component example it's finished. included watch property in setValue function

--- pages/components/6NameFormHookFormDetailed.js
+++ pages/components/6NameFormHookFormDetailed.js
@@ -3,7 +3,11 @@ import { useEffect } from 'react'
 
 export default function NameFormHookFormDetailed() {
 
-    const {register, handleSubmit, setValue} = useForm({
+    // register: for taking each object property on included in the <input> field for TODO or update
+    // handleSubmit: to pass the object into another function that'll console.log() the data
+    // setValue: to set the default value of the object
+    // watch: it's to watch the property of the user while typing in the field, which in our case we used it in fullName field
+    const {register, handleSubmit, setValue, watch} = useForm({
         defaultValues: {
             fullName: "",
             address: "",
@@ -14,11 +18,16 @@ export default function NameFormHookFormDetailed() {
         },
     });
 
+    // Here, we included a watch property at line 10 in useForm() field. This will help the user see its typing fullName in <input> field
+    //which we included on line 56
+    const watchUserName = watch("fullName");
+
     useEffect(() => {
+        // here this data{} object will be the default value
         let data = {
             fullName: "Homer Simpson",
             address: "123 Main St, Springfield U.S.A",
-            campus: "Newnham",
+            campus: "newnham",
             program: ["CPA", "CPP"],
             parking: "Semester",
             active: true
@@ -32,15 +41,48 @@ export default function NameFormHookFormDetailed() {
 
 
     function submitForm(data) {
+        console.log("TODO, submit this to an API endpoint");
+        console.log(JSON.stringify(data));
         console.log(data);
     }
+
+
     return (
         <>
             <h3>6.NameFormHookFormDetailed</h3>
 
             <form onSubmit={handleSubmit(submitForm)}>
                 Full Name: <br />
+                {watchUserName} <br />
                 <input {...register("fullName")} ></input> <br /><br />
+
+                Address: <br />
+                <textarea {...register("address")}></textarea> <br /><br />
+
+                Campus: <br />
+                <select {...register("campus")}>
+                    <option value="king">King</option>
+                    <option value="satY">Seneca ar York</option>
+                    <option value="newnham">Newnham</option>
+                    <option value="markham">Markham</option>
+                </select> <br /><br />
+
+                Program: <br />
+                <select multiple {...register("program")}>
+                    <option value="DAD">Data Application Developer</option>
+                    <option value="CPA">Computer Programming &amp; Analysis</option>
+                    <option value="CPP">Data Application Developer</option>
+                    <option value="DSA">Data Application Developer</option>
+                </select> <br /><br />
+
+                Parking: <br />
+                <input type="radio" value="daily" {...register("parking")}/> Daily <br />
+                <input type="radio" value="semester" {...register("parking")} /> Semester<br />
+                <input type="radio" value="year" {...register("parking")}/> Academic Year <br /><br />
+
+                Active: <br />
+                <input type="checkbox" {...register("active")} /> Currently Active <br /><br />
+
                 <button type="submit">UpdateUser</button>
 
             </form>
